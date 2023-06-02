7964c9e05ebbcf583b2633913bb5a2d4c20a60e0
Daniel Felipe <daniell.felipe.2017@gmail.com>
2023-06-02 07:43:40 -0300

    changing folder structure

--- install.sh
+++ install.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-SCRIPT_ROOT=$(pwd)
+# colors
+green=$(tput setaf 1)
+none=$(tput sgr0)
+
+SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+echo "[${green}+${none}] Copying dotfiles"
 
 # startship
 [ ! -d "$HOME/.config/startship/" ] && mkdir "$HOME/.config/starship"
-cp "$SCRIPT_ROOT/startship/starship.toml" "$HOME/.config/starship/" 
+cp "$SCRIPT_ROOT/config/startship/starship.toml" "$HOME/.config/starship/" 
 
 # zsh
 [ -f "$HOME/.zshrc" ] && mv "$HOME/.zshrc" "$HOME/.zshrc.backup"
-cp "$SCRIPT_ROOT/zsh/zshrc" "$HOME/.zshrc"
+cp "$SCRIPT_ROOT/config/zsh/zshrc" "$HOME/.zshrc"
 
 # i3
 if [ -f "$HOME/.config/i3/config" ]; then
     mv "$HOME/.config/i3/config" "$HOME/.config/i3/config.backup"
 fi
-cp -R "$SCRIPT_ROOT/i3" "$HOME/.config/"
+cp -R "$SCRIPT_ROOT/config/i3" "$HOME/.config/"
 
 # i3status
 if [ -f "$HOME/.config/i3/i3status.conf"]; then
     mv "$HOME/.config/i3/i3status.conf" "$HOME/.config/i3/i3status.conf.backup"
 fi
-cp "$SCRIPT_ROOT/i3status/i3status.conf" "$HOME/.config/i3/"
+cp "$SCRIPT_ROOT/config/i3status/i3status.conf" "$HOME/.config/i3/"
+
+echo "${green}Done!${none}"
